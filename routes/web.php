0d061010f18ed24dab67e6afb91b87661375b46f
taufiq <taufiqya98@gmail.com>
2022-09-08 07:31:44 +0700

    Login authentic & Logout, status : Succes

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
 
 Route::get('login',[AuthController::class,'index'])->name('login');
 Route::post('proses_login',[AuthController::class,'proses_login'])->name('proses_login');
-Route::get('/logout',[AuthController::class,'logout'])->name('logout');
+Route::get('logout',[AuthController::class,'logout'])->name('logout');
 
 Route::group(['middleware' => ['auth']], function() {
     Route::group(['middleware' => ['Cek_login:admin']], function() {
